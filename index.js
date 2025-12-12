2b08f7929e3f9047e0519244b15fbee0f8b9c22c
injamul007 <injamul007hoque@gmail.com>
2025-12-13 02:40:47 +0600

    connected to mongodb successfully with mongodb secret credencial store in environment variables

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
+require("dotenv").config();
 const express = require('express');
 const cors = require('cors');
-const port = process.env.PORT || 3000;
 const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT || 3000;
 
 //? middlewares
 app.use(express.json())
 app.use(cors())
+
+const uri = process.env.URI;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
 
 app.get('/', (req,res) => {
   res.status(200).json({
@@ -13,6 +25,21 @@ app.get('/', (req,res) => {
     message: "Microloan Server is Running Fine"
   })
 })
+
+
+async function run() {
+  try {
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+  }
+}
+run().catch(console.dir);
+
+
 
 app.listen(port, () => {
   console.log(`Microloan Server is Running on Port: ${port}`)
