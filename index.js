18814039c957f73a10f345215d9370c4fb756675
injamul007 <injamul007hoque@gmail.com>
2025-12-14 09:06:42 +0600

    add available loan api by show on home

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 require("dotenv").config();
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 3000;
 
 //? middlewares
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 const uri = process.env.URI;
 
@@ -16,20 +16,43 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
-app.get('/', (req,res) => {
+app.get("/", (req, res) => {
   res.status(200).json({
     status: true,
-    message: "Microloan Server is Running Fine"
-  })
-})
-
+    message: "Microloan Server is Running Fine",
+  });
+});
 
 async function run() {
   try {
     await client.connect();
+
+    //? Database and collection setup
+    const db = client.db("microloanDB");
+    const loansCollection = db.collection("loans");
+
+    //? available loans get api by show on home with limit
+    app.get("/available-loans", async (req, res) => {
+      try {
+        const query = { show_on_home: true };
+        const result = await loansCollection.find(query).limit(6).toArray();
+        res.status(200).json({
+          status: true,
+          message: "Available loans get api successful",
+          result,
+        })
+      } catch (error) {
+        res.status(500).json({
+          status: false,
+          message: 'Failed to get api for available loans',
+          error: error.message,
+        })
+      }
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Successfully connected to MongoDB!");
@@ -39,8 +62,6 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
 app.listen(port, () => {
-  console.log(`Microloan Server is Running on Port: ${port}`)
-})
+  console.log(`Microloan Server is Running on Port: ${port}`);
+});
