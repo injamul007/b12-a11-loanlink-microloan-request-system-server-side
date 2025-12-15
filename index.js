95ae7fce9d257fecb945df80986989c443f55472
injamul007 <injamul007hoque@gmail.com>
2025-12-15 07:17:52 +0600

    add service key from firebase and also store the firebase service key in .env variables and also add JWT verify middleware

--- index.js
+++ index.js
@@ -3,11 +3,43 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const admin = require("firebase-admin");
 const port = process.env.PORT || 3000;
 
+const decoded = Buffer.from(`${process.env.FB_SERVICE_KEY}`, "base64").toString(
+  "utf-8"
+);
+
+const serviceAccount = JSON.parse(decoded);
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
 //? middlewares
+app.use(
+  cors({
+    origin: [process.env.CLIENT_DOMAIN_URL],
+    credentials: true,
+    optionSuccessStatus: 200,
+  })
+);
 app.use(express.json());
-app.use(cors());
+
+// JWT middlewares
+const verifyJWT = async (req, res, next) => {
+  const token = req?.headers?.authorization?.split(" ")[1];
+  console.log(token);
+  if (!token) return res.status(401).send({ message: "Unauthorized Access!" });
+  try {
+    const decoded = await admin.auth().verifyIdToken(token);
+    req.tokenEmail = decoded.email;
+    console.log(decoded);
+    next();
+  } catch (err) {
+    console.log(err);
+    return res.status(401).send({ message: "Unauthorized Access!", err });
+  }
+};
 
 const uri = process.env.URI;
 
@@ -33,62 +65,62 @@ async function run() {
     //? Database and collection setup
     const db = client.db("microloanDB");
     const loansCollection = db.collection("loans");
-    const usersCollection = db.collection('users')
-
+    const usersCollection = db.collection("users");
 
     //? save users data in db
-    app.post('/users', async(req,res) => {
+    app.post("/users", async (req, res) => {
       try {
-        const userData = req.body
+        const userData = req.body;
 
         //? validate user email and name
-        if(!userData?.email || !userData?.name) {
+        if (!userData?.email || !userData?.name) {
           return res.status(400).json({
             status: false,
-            message: "User email and name is required"
-          })
+            message: "User email and name is required",
+          });
         }
 
-        let allowedRoles = ['borrower', 'manager']
-        let role = allowedRoles.includes(userData.role) ? userData.role : 'borrower'
-        
+        let allowedRoles = ["borrower", "manager"];
+        let role = allowedRoles.includes(userData.role)
+          ? userData.role
+          : "borrower";
+
         userData.role = role;
         userData.created_At = new Date();
         userData.last_loggedIn = new Date();
 
-        const query = {email: userData.email}
-        const exitingUser = await usersCollection.findOne(query)
-        
+        const query = { email: userData.email };
+        const exitingUser = await usersCollection.findOne(query);
+
         //? validate the user is already stored in db or not
-        if(exitingUser) {
+        if (exitingUser) {
           const update = {
             $set: {
               last_loggedIn: new Date(),
-            }
-          }
-          const updateUser = await usersCollection.updateOne(query, update)
+            },
+          };
+          const updateUser = await usersCollection.updateOne(query, update);
           return res.status(200).json({
             status: true,
-            message: 'User Already Exits and last loggedIn updated',
+            message: "User Already Exits and last loggedIn updated",
             updateUser,
-          })
+          });
         }
 
-        const result = await usersCollection.insertOne(userData)
+        const result = await usersCollection.insertOne(userData);
         res.status(201).json({
           status: false,
           message: "All the users data save in db successful",
           result,
-        })
-
+        });
       } catch (error) {
         res.status(500).json({
           status: false,
           message: "Failed to post users data on db",
           error: error.message,
-        })
+        });
       }
-    })
+    });
 
     //? available loans get api by show on home with limit
     app.get("/available-loans", async (req, res) => {
@@ -99,70 +131,69 @@ async function run() {
           status: true,
           message: "Available loans get api successful",
           result,
-        })
+        });
       } catch (error) {
         res.status(500).json({
           status: false,
-          message: 'Failed to get api for available loans',
+          message: "Failed to get api for available loans",
           error: error.message,
-        })
+        });
       }
     });
 
     //? get api for all loans in the all loans page
-    app.get('/all-loans', async(req,res) => {
+    app.get("/all-loans", async (req, res) => {
       try {
-        const result = await loansCollection.find().toArray()
+        const result = await loansCollection.find().toArray();
         res.status(200).json({
           status: true,
-          message: 'Get all loans api successful',
+          message: "Get all loans api successful",
           result,
-        })
+        });
       } catch (error) {
         res.status(500).json({
           status: false,
           message: "Failed to get all the loans in all loans page",
           error: error.message,
-        })
+        });
       }
-    })
+    });
 
     //? single get api by calling its id
-    app.get('/all-loans/:id', async(req,res) => {
+    app.get("/all-loans/:id", async (req, res) => {
       try {
         const loanId = req.params.id;
 
-        if(!ObjectId.isValid(loanId)) {
+        if (!ObjectId.isValid(loanId)) {
           return res.status(400).json({
             status: false,
             message: "Invalid Object id",
-          })
+          });
         }
 
-        const query = {_id: new ObjectId(loanId)}
-        const result = await loansCollection.findOne(query)
+        const query = { _id: new ObjectId(loanId) };
+        const result = await loansCollection.findOne(query);
 
-        if(!result) {
+        if (!result) {
           return res.status(404).json({
             status: false,
             message: "Loan Not Found",
-          })
+          });
         }
 
         res.status(200).json({
           status: true,
           message: "Single loan api data by id successful",
           result,
-        })
-
+        });
       } catch (error) {
         res.status(500).json({
           status: false,
-          message: 'Failed to get single loan api data by id from db',
+          message: "Failed to get single loan api data by id from db",
           error: error.message,
-        })
+        });
       }
-    })
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
