8699c2944000d7ad07d79dc69906e392a0a25165
injamul007 <injamul007hoque@gmail.com>
2025-12-23 01:40:02 +0600

    comment uncomment some mongodb connecting related code just for to connect with db for client side issue

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ app.get("/", (req, res) => {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
 
     //? Database and collection setup
     const db = client.db("loanLinkDB");
@@ -1021,8 +1021,8 @@ async function run() {
     );
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
   }
