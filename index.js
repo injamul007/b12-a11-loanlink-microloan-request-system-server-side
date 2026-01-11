7c3fdd3d9ec4179d6504b5756a98519f6dca8793
injamul007 <injamul007hoque@gmail.com>
2026-01-12 03:12:54 +0600

    update and add filter query in query params and set it to find in get all loans api

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ app.get("/", (req, res) => {
 
 async function run() {
   try {
-    // await client.connect();
+    await client.connect();
 
     //? Database and collection setup
     const db = client.db("loanLinkDB");
@@ -215,11 +215,18 @@ async function run() {
       try {
         const page = Number(req.query.page) || 1;
         const limit = Number(req.query.limit) || 10;
+        const category = req.query.category || "";
+        console.log(category)
 
         const skip = (page - 1) * limit;
 
+        const query = {};
+        if(category) {
+          query.category = { $regex: category, $options: "i" }
+        }
+
         const result = await loansCollection
-          .find()
+          .find(query)
           .skip(skip)
           .limit(limit)
           .toArray();
@@ -1021,8 +1028,8 @@ async function run() {
     );
 
     // Send a ping to confirm a successful connection
-    // await client.db("admin").command({ ping: 1 });
-    // console.log("Successfully connected to MongoDB!");
+    await client.db("admin").command({ ping: 1 });
+    console.log("Successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
   }
